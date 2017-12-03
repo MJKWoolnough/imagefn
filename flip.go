6a674d3dfebda6ee76999cb81d740c76842122e2
Michael Woolnough <michael.woolnough@gmail.com>
2017-12-03 13:36:05 +0000

    added flips

---
+++ flip.go
@@ -0,0 +1,42 @@
+package imagefn
+
+import (
+	"image"
+	"image/color"
+)
+
+type FlipX struct {
+	image.Image
+}
+
+func (f *FlipX) At(x, y int) color.Color {
+	b := f.Bounds()
+	return f.Image.At(b.Max.X+b.Min.X-x, y)
+}
+
+func (f *FlipX) SubImage(r image.Rectangle) image.Image {
+	b := f.Bounds()
+	r.Min.X = b.Max.X + b.Min.X - r.Min.X
+	r.Max.X = b.Max.X + b.Min.X - r.Max.X
+	return &FlipX{
+		Image: SubImage(r.Image, r.Canon()),
+	}
+}
+
+type FlipY struct {
+	image.Image
+}
+
+func (f *FlipY) At(x, y int) color.Color {
+	b := f.Bounds()
+	return f.Image.At(x, b.Max.Y+b.Min.Y-y)
+}
+
+func (f *FlipY) SubImage(r image.Rectangle) image.Image {
+	b := f.Bounds()
+	r.Min.Y = b.Max.Y + b.Min.Y - r.Min.Y
+	r.Max.Y = b.Max.Y + b.Min.Y - r.Max.Y
+	return &FlipY{
+		Image: SubImage(r.Image, r.Canon()),
+	}
+}
