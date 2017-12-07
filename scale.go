9ba6bb85e222342e7db5f8ee0625ab8eab73fed8
Michael Woolnough <michael.woolnough@gmail.com>
2017-12-07 09:38:01 +0000

    need to divide, not multiply, the scale to get the original coords/bounds

--- scale.go
+++ scale.go
@@ -15,15 +15,15 @@ func (s *Scale) Bounds() image.Rectangle {
 	return image.Rect(
 		b.Min.X,
 		b.Min.Y,
-		b.Min.X+int(float64(b.Dx())*s.ScaleX),
-		b.Min.Y+int(float64(b.Dy())*s.ScaleY),
+		b.Min.X+int(float64(b.Dx())/s.ScaleX),
+		b.Min.Y+int(float64(b.Dy())/s.ScaleY),
 	)
 }
 
 func (s *Scale) At(x, y int) color.Color {
 	b := s.Image.Bounds()
 	return s.Image.At(
-		b.Min.X+int(float64(x-b.Min.X)*s.ScaleX),
-		b.Min.Y+int(float64(y-b.Min.Y)*s.ScaleY),
+		b.Min.X+int(float64(x-b.Min.X)/s.ScaleX),
+		b.Min.Y+int(float64(y-b.Min.Y)/s.ScaleY),
 	)
 }
