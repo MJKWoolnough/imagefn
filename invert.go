70f9eb59df801f975519c559a5e219ed6fb545bb
Michael Woolnough <michael.woolnough@gmail.com>
2017-12-11 13:02:52 +0000

    added SubImage method for Invert

--- invert.go
+++ invert.go
@@ -12,9 +12,13 @@ type Invert struct {
 func (i Invert) At(x, y int) color.Color {
 	r, g, b, a := i.Image.At(x, y).RGBA()
 	return i.Image.ColorModel().Convert(color.RGBA64{
-		R: a - r,
-		G: a - g,
-		B: a - b,
-		A: a,
+		R: uint16(a - r),
+		G: uint16(a - g),
+		B: uint16(a - b),
+		A: uint16(a),
 	})
+}
+
+func (i Invert) SubImage(r image.Rectangle) image.Image {
+	return Invert{SubImage(i.Image, r)}
 }
