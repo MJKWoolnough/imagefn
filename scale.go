0cb31e1aa6efd379a466ab2aa09d940397356d2f
Michael Woolnough <michael.woolnough@gmail.com>
2018-01-14 17:22:37 +0000

    added type/scale checking for Scale

--- scale.go
+++ scale.go
@@ -5,12 +5,52 @@ import (
 	"image/color"
 )
 
-type Scale struct {
+type scale struct {
 	image.Image
 	ScaleX, ScaleY float64
 }
 
-func (s *Scale) Bounds() image.Rectangle {
+func Scale(i image.Image, xScale, yScale float64) image.Image {
+	switch im := i.(type) {
+	case *scale:
+		xScale *= im.ScaleX
+		yScale *= im.ScaleY
+		i = im.Image
+	}
+	if xScale < 0 {
+		if yScale < 0 {
+			i = FlipX(i)
+		} else {
+			i = Rotate180(i)
+			yScale = -yScale
+		}
+		xScale = -xScale
+	} else if yScale < 0 {
+		i = FlipY(i)
+		yScale = -yScale
+	}
+	if xScale == 0 || yScale == 0 {
+		return empty{
+			Model: i.ColorModel(),
+			Min:   i.Bounds().Min,
+		}
+	}
+	if xScale == 1 && yScale == 1 {
+		return i
+	}
+	return &scale{
+		Image:  i,
+		ScaleX: xScale,
+		ScaleY: yScale,
+	}
+}
+
+func ScaleDimensions(i image.Image, x, y int) image.Image {
+	b := i.Bounds()
+	return Scale(i, float64(x)/float64(b.Dx()), float64(y)/float64(b.Dy()))
+}
+
+func (s *scale) Bounds() image.Rectangle {
 	b := s.Image.Bounds()
 	return image.Rect(
 		b.Min.X,
@@ -20,7 +60,7 @@ func (s *Scale) Bounds() image.Rectangle {
 	)
 }
 
-func (s *Scale) At(x, y int) color.Color {
+func (s *scale) At(x, y int) color.Color {
 	b := s.Image.Bounds()
 	return s.Image.At(
 		b.Min.X+int(float64(x-b.Min.X)/s.ScaleX),
